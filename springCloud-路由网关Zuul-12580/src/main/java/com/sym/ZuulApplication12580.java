aa7abf93bb5416e7393ba1b6c7909ecb47883c6c
sym <542569199@qq.com>
2019-03-03 20:48:18 +0800

    路由官网zuul

---
+++ springCloud-路由网关Zuul-12580/src/main/java/com/sym/ZuulApplication12580.java
@@ -0,0 +1,16 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by 沈燕明 on 2019/3/3.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication12580 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication12580.class,args);
+    }
+}
