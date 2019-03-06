22704ef994844265b0daab2df46bd2c52fde147e
sym <542569199@qq.com>
2019-03-06 22:42:57 +0800

    分布式配置中心config的服务端

---
+++ springCloud-配置中心-configServer-10086/src/main/java/com/sym/ConfigServer10086.java
@@ -0,0 +1,16 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by 沈燕明 on 2019/3/6.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer10086 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer10086.class,args);
+    }
+}
