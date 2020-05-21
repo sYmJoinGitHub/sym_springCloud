839904d0066854d740c09766f5e2d058ff460b53
shenyanming <sym542569199@gmail.com>
2020-05-21 21:02:20 +0800

    Bus & Config server

--- springCloud-配置中心&消息总线-configServer/src/main/java/com/sym/config/MyPropertySourceLoader.java
+++ springCloud-配置中心&消息总线-configServer/src/main/java/com/sym/config/MyPropertySourceLoader.java
@@ -8,6 +8,9 @@ import org.springframework.core.io.Resource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -25,39 +28,37 @@ public class MyPropertySourceLoader implements PropertySourceLoader {
     }
 
     @Override
-    public PropertySource<?> load(String name, Resource resource, String profile) throws IOException {
-        if (profile == null) {
+    public List<PropertySource<?>> load(String name, Resource resource) throws IOException {
+        try {
+            // spring cloud config 就是使用Properties去解析配置文件的
+            Properties prop = new Properties();
+            InputStream is = resource.getInputStream();
+            InputStreamReader isr = null;
             try {
-                // spring cloud config 就是使用Properties去解析配置文件的
-                Properties prop = new Properties();
-                InputStream is = resource.getInputStream();
-                InputStreamReader isr = null;
-                try {
-                    String fileName = resource.getFilename();
-                    if (null != fileName && fileName.endsWith(".xml")) {
-                        // xml默认是以utf-8去解析的
-                        prop.loadFromXML(is);
-                    } else {
-                        // .properties默认是以iso-8859-1解析，我们重新处理，让他以utf-8的编码格式去解析
-                        isr = new InputStreamReader(is, "utf-8");
-                        prop.load(isr);
-                    }
-                    // 如果有解析到配置文件再返回PropertiesPropertySource对象
-                    if (!prop.isEmpty()) {
-                        return new PropertiesPropertySource(name, prop);
-                    }
-                } finally {
-                    // 如果开启了 InputStreamReader，我们直接关闭它会连带关闭 InputStream
-                    if (null != isr) {
-                        isr.close();
-                    } else {
-                        // 否则我们需要手动关闭InputStream
-                        is.close();
-                    }
+                String fileName = resource.getFilename();
+                if (null != fileName && fileName.endsWith(".xml")) {
+                    // xml默认是以utf-8去解析的
+                    prop.loadFromXML(is);
+                } else {
+                    // .properties默认是以iso-8859-1解析，我们重新处理，让他以utf-8的编码格式去解析
+                    isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                    prop.load(isr);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+                // 如果有解析到配置文件再返回PropertiesPropertySource对象
+                if (!prop.isEmpty()) {
+                    return Collections.singletonList(new PropertiesPropertySource(name, prop));
+                }
+            } finally {
+                // 如果开启了 InputStreamReader，我们直接关闭它会连带关闭 InputStream
+                if (null != isr) {
+                    isr.close();
+                } else {
+                    // 否则我们需要手动关闭InputStream
+                    is.close();
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
