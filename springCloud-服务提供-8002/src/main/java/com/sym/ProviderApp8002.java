a84977c7c5d7829352b7a8bae206499e5086c974
shenym <542569199@qq.com>
2020-05-21 16:14:49 +0800

    provider

--- springCloud-服务提供-8002/src/main/java/com/sym/ProviderApp8002.java
+++ springCloud-服务提供-8002/src/main/java/com/sym/ProviderApp8002.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Created by 沈燕明 on 2019/1/27.
+ *
+ * @author shenyanming
+ * @date 2019/1/27
  */
 @SpringBootApplication
 @EnableEurekaClient
--- springCloud-服务提供-8002/src/main/java/com/sym/controller/UserController.java
+++ springCloud-服务提供-8002/src/main/java/com/sym/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.sym.controller;
 
 import com.sym.entity.UserBean;
-import com.sym.service.UserServiceI;
+import com.sym.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,17 @@ import java.util.List;
 
 /**
  * 因为springCloud是基于REST调用服务的，所以服务提供时，要写在Controller层
- * <p>
- * Created by 沈燕明 on 2019/1/20.
+ *
+ * @author shenyanming
+ * @date 2019/1/20
  */
 @RestController
-@RequestMapping("provider/user")
+@RequestMapping("/provider/user")
+@Slf4j
 public class UserController {
 
     @Autowired
-    private UserServiceI userService;
+    private IUserService userService;
 
     /**
      * 用于服务发现功能，了解下有这个组件即可
@@ -31,53 +35,44 @@ public class UserController {
 
     /**
      * 获取单个用户信息
-     *
-     * @param userId
-     * @return
-     * @throws Exception
      */
-    @RequestMapping("get/{id}")
-    public UserBean getUserById(@PathVariable("id") int userId) throws Exception {
+    @GetMapping("/get/{id}")
+    public UserBean getUserById(@PathVariable("id") int userId) {
+        log.info("[8002]获取单个用户信息：{}", userId);
         return userService.getUserById(userId);
     }
 
     /**
      * 获取所有用户的信息
-     *
-     * @return
-     * @throws Exception
      */
-    @RequestMapping("list")
-    public List<UserBean> getUserList() throws Exception {
+    @GetMapping("/list")
+    public List<UserBean> getUserList() {
+        log.info("[8002]获取所有用户信息");
         return userService.getUserList();
     }
 
     /**
      * 服务提供方抛出异常，供消费者测试服务熔断
-     *
-     * @return
-     * @throws Exception
      */
-    @RequestMapping("error")
-    public UserBean error() throws Exception {
+    @RequestMapping("/error")
+    public UserBean error() {
+        log.error("[8002]模拟异常");
         throw new RuntimeException("服务提供-8002-抛出异常");
     }
 
     /**
      * 服务发现功能，了解下有这个功能即可
      * 提供提供方的服务功能信息
-     *
-     * @return
-     * @throws Exception
      */
-    @RequestMapping("discovery")
-    public DiscoveryClient DiscoveryClient() throws Exception {
+    @RequestMapping("/discovery")
+    public DiscoveryClient discoveryClient() {
+        log.info("[8002]服务发现");
         // 获取所有的服务信息
         List<String> services = discoveryClient.getServices();
         System.out.println("所有服务信息：" + services);
         // 获取本服务信息，参数本服务的应用名称
         List<ServiceInstance> instances = discoveryClient.getInstances("springCloud-provider");
-        instances.stream().forEach((a) -> {
+        instances.forEach((a) -> {
             System.out.println(a.getHost() + "\t" + a.getPort() + "\t" + a.getUri() + a.getServiceId() + "\t" + a.getMetadata());
         });
         return discoveryClient;
