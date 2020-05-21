6c460c4d1c5eaa81643bc939627ec2b1159a2aa3
shenym <542569199@qq.com>
2020-05-21 16:17:05 +0800

    bus & Config client

--- springCloud-配置中心&消息总线-configClient/src/main/java/com/sym/controller/ConfigClientController.java
+++ springCloud-配置中心&消息总线-configClient/src/main/java/com/sym/controller/ConfigClientController.java
@@ -2,7 +2,6 @@ package com.sym.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
