926089665c7417c53b45ba25d37c521ae0ce77eb
shenym <542569199@qq.com>
2020-05-21 20:50:40 +0800

    feign

--- springCloud-服务消费-feign/src/main/java/com/sym/FeignApplication.java
+++ springCloud-服务消费-feign/src/main/java/com/sym/FeignApplication.java
@@ -2,6 +2,8 @@ package com.sym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author shenyanming
  * @date 2019/1/20
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {EurekaClientAutoConfiguration.class, EurekaDiscoveryClientConfiguration.class})
 @EnableFeignClients
 public class FeignApplication {
     public static void main(String[] args) {
