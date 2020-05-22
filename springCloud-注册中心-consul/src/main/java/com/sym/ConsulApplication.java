3c083044787e7a13876e6b3856fe6372187199bf
shenym <542569199@qq.com>
2020-05-22 11:34:08 +0800

    Consul

--- springCloud-注册中心-consul/src/main/java/com/sym/ConsulApplication.java
+++ springCloud-注册中心-consul/src/main/java/com/sym/ConsulApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * Consul 的方案更“一站式”，内置了服务注册与发现框架、分布一致性协议实现、健康检查、Key/Value 存储、多数据中心方案，
  * 不再需要依赖其它工具（比如 ZooKeeper 等）。
  *
- * Consul 与 Eureka不同, 它需要独立部署, 所以需要先安装, 官网下载地址为：
+ * Consul 与 Eureka不同, 它的服务端需要单独部署, 所以需要先安装, 官网下载地址为：
  * https://www.consul.io/downloads.html
  *
  * @author shenyanming
