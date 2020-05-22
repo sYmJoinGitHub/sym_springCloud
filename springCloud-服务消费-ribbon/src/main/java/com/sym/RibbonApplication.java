23e1b0f51813ab7952eeb00090444cb8b7bec4f2
shenyanming <sym542569199@gmail.com>
2020-05-22 22:20:35 +0800

    ribbon-consumer, 集成Eureka、Consul、Nacos

--- springCloud-服务消费-ribbon/src/main/java/com/sym/RibbonApplication.java
+++ springCloud-服务消费-ribbon/src/main/java/com/sym/RibbonApplication.java
@@ -2,15 +2,7 @@ package com.sym;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.serviceregistry.AutoServiceRegistrationProperties;
-import org.springframework.cloud.consul.discovery.ConsulRibbonClientConfiguration;
-import org.springframework.cloud.netflix.ribbon.eureka.EurekaRibbonClientConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
 /**
  * 基于Ribbon的springCloud服务消费端
@@ -21,12 +13,6 @@ import org.springframework.context.annotation.FilterType;
 @SpringBootApplication
 @Slf4j
 public class RibbonApplication {
-
-    @Bean
-    public AutoServiceRegistrationProperties autoServiceRegistrationProperties(){
-        return new AutoServiceRegistrationProperties();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(RibbonApplication.class, args);
         log.info("ribbon consumer 启动完成");
--- springCloud-服务消费-ribbon/src/main/java/com/sym/controller/RibbonController.java
+++ springCloud-服务消费-ribbon/src/main/java/com/sym/controller/RibbonController.java
@@ -3,6 +3,8 @@ package com.sym.controller;
 import com.sym.entity.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,9 @@ public class RibbonController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     /**
      * 调用远程服务，获取当个用户的信息
      */
@@ -47,4 +52,19 @@ public class RibbonController {
         return restTemplate.getForObject(url, List.class);
     }
 
+    /**
+     * 获取所有服务
+     */
+    @GetMapping("/services")
+    public List<String> getServices(){
+        return discoveryClient.getServices();
+    }
+
+    /**
+     * 获取指定服务的实例信息
+     */
+    @GetMapping("/instance/{serviceId}")
+    public List<ServiceInstance> getInstance(@PathVariable("serviceId") String serviceId){
+        return discoveryClient.getInstances(serviceId);
+    }
 }
