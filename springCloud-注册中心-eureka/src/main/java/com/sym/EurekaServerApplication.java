638cfd06a4e17aaf51fc6df1c3f7e9834f9abe14
shenym <542569199@qq.com>
2020-05-22 11:30:14 +0800

    Eureka

--- springCloud-注册中心-eureka/src/main/java/com/sym/EurekaServerApplication.java
+++ springCloud-注册中心-eureka/src/main/java/com/sym/EurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.sym;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,8 +12,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        log.info("Eureka Server started");
     }
 }
