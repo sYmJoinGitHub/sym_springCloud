29aa23e285a2d3b209a84d400fbcc9a7d86503ca
shenyanming <sym542569199@gmail.com>
2020-05-24 22:50:43 +0800

    熔断器Sentinel

---
+++ springCloud-熔断器-sentinel/src/main/java/com/sym/SentinelApplication.java
@@ -0,0 +1,15 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shenyanming
+ * @date 2020/5/24 22:44.
+ */
+@SpringBootApplication
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+}
