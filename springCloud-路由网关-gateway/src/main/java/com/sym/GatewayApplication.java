eb93913a1db6728d69559882c07143e9a0879eab
shenyanming <sym542569199@gmail.com>
2020-05-24 22:50:55 +0800

    路由网关gateway

---
+++ springCloud-路由网关-gateway/src/main/java/com/sym/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.sym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shenyanming
+ * @date 2020/5/24 22:45.
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
